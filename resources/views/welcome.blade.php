321b53fbc7fe3a882cecbaa14a0f76c5500ebfad
esimonor <esimonor18dw@ikzubirimanteo.com>
2019-11-26 11:35:07 +0100

    Landing page final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap CSS -->
 		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-		<!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
 		<style>
 			li:hover{
 				text-decoration: underline;
@@ -99,13 +98,10 @@
 
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<!--<script src={{ asset('js/jquery-3.4.1.min.js') }}"></script>
+	<script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
 	<script src="{{ asset('js/popper.min.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.min.js') }}"></script>-->
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
 </body>
 </html>
